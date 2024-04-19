78343d683f641232ca4e2ed3e123b495c2bb5f06
dkwelcher <dkwelcher@gmail.com>
2024-04-19 15:57:25 -0400

    Add lombok annotations; removed constuctors / getters, setters

--- src/main/java/com/flightplanner/flightplannerserver/domain/dto/AircraftDto.java
+++ src/main/java/com/flightplanner/flightplannerserver/domain/dto/AircraftDto.java
@@ -1,5 +1,14 @@
 package com.flightplanner.flightplannerserver.domain.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class AircraftDto {
 
     private long aircraftId;
@@ -11,85 +20,4 @@ public class AircraftDto {
     private double airspeed; // measured in knots at cruise speed
     private double range; // measured in nautical miles
     private double maximumRunwayLength; // measured in feet
-
-    public AircraftDto(long aircraftId, String aircraftMake, String aircraftModel, String aircraftType, double fuelTankSize,
-                       double fuelBurnRate, double airspeed, double range, double maximumRunwayLength) {
-        this.aircraftId = aircraftId;
-        this.aircraftMake = aircraftMake;
-        this.aircraftModel = aircraftModel;
-        this.aircraftType = aircraftType;
-        this.fuelTankSize = fuelTankSize;
-        this.fuelBurnRate = fuelBurnRate;
-        this.airspeed = airspeed;
-        this.range = range;
-        this.maximumRunwayLength = maximumRunwayLength;
-    }
-
-    public long getAircraftId() {
-        return aircraftId;
-    }
-
-    public void setAircraftId(long aircraftId) {
-        this.aircraftId = aircraftId;
-    }
-
-    public String getAircraftMake() {
-        return aircraftMake;
-    }
-
-    public void setAircraftMake(String aircraftMake) {
-        this.aircraftMake = aircraftMake;
-    }
-
-    public String getAircraftModel() {
-        return aircraftModel;
-    }
-
-    public void setAircraftModel(String aircraftModel) {
-        this.aircraftModel = aircraftModel;
-    }
-
-    public String getAircraftType() {
-        return aircraftType;
-    }
-
-    public void setAircraftType(String aircraftType) {
-        this.aircraftType = aircraftType;
-    }
-
-    public double getFuelTankSize() {
-        return fuelTankSize;
-    }
-
-    public void setFuelTankSize(double fuelTankSize) {
-        this.fuelTankSize = fuelTankSize;
-    }
-
-    public double getFuelBurnRate() {
-        return fuelBurnRate;
-    }
-
-    public void setFuelBurnRate(double fuelBurnRate) {
-        this.fuelBurnRate = fuelBurnRate;
-    }
-
-    public double getAirspeed() {
-        return airspeed;
-    }
-
-    public void setAirspeed(double airspeed) {
-        this.airspeed = airspeed;
-    }
-
-    public double getRange() { return range; }
-
-    public void setRange(double range) { this.range = range; }
-
-    public double getMaximumRunwayLength() {
-        return maximumRunwayLength;
-    }
-
-    public void setMaximumRunwayLength(double maximumRunwayLength) {
-        this.maximumRunwayLength = maximumRunwayLength;
-    }
 }
