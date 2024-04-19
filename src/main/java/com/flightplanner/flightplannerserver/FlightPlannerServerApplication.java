474d827ef2eb81ea8fc1d2962a250254fa57b0c5
dkwelcher <dkwelcher@gmail.com>
2024-04-19 10:16:06 -0400

    Initial commit

---
+++ src/main/java/com/flightplanner/flightplannerserver/FlightPlannerServerApplication.java
@@ -0,0 +1,13 @@
+package com.flightplanner.flightplannerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightPlannerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightPlannerServerApplication.class, args);
+	}
+
+}
