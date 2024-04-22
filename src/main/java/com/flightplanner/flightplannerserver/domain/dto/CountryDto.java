e4b8c68cb246eb0ecbac549db75e0ac30b2208f8
dkwelcher <dkwelcher@gmail.com>
2024-04-22 14:35:59 -0400

    Added DTO for Country objects

---
+++ src/main/java/com/flightplanner/flightplannerserver/domain/dto/CountryDto.java
@@ -0,0 +1,17 @@
+package com.flightplanner.flightplannerserver.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CountryDto {
+
+    private long countryId;
+    private String countryName;
+    private ContinentDto continentDto;
+}
