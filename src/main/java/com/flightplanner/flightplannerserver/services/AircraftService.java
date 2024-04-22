57606d72688d15d62fe9621391c737ae72967bcb
dkwelcher <dkwelcher@gmail.com>
2024-04-22 14:29:22 -0400

    Removed unused import

--- src/main/java/com/flightplanner/flightplannerserver/services/AircraftService.java
+++ src/main/java/com/flightplanner/flightplannerserver/services/AircraftService.java
@@ -1,7 +1,6 @@
 package com.flightplanner.flightplannerserver.services;
 
 import com.flightplanner.flightplannerserver.domain.dto.AircraftDto;
-import com.flightplanner.flightplannerserver.domain.entities.AircraftEntity;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,14 +8,9 @@ import java.util.Optional;
 public interface AircraftService {
 
     AircraftDto save(AircraftDto aircraftDto);
-
     AircraftDto update(AircraftDto aircraftDto);
-
     Optional<AircraftDto> findById(Long aircraftId);
-
     List<AircraftDto> findAll();
-
     boolean existsById(Long aircraftId);
-
     void deleteById(Long aircraftId);
 }
