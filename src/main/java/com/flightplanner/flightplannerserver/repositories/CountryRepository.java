e73bf000966db4645a3596c0c12eb6bfc03c87be
dkwelcher <dkwelcher@gmail.com>
2024-04-23 14:35:03 -0400

    Added method for finding country entity by searching for country name

--- src/main/java/com/flightplanner/flightplannerserver/repositories/CountryRepository.java
+++ src/main/java/com/flightplanner/flightplannerserver/repositories/CountryRepository.java
@@ -1,5 +1,6 @@
 package com.flightplanner.flightplannerserver.repositories;
 
+import com.flightplanner.flightplannerserver.domain.dto.CountryDto;
 import com.flightplanner.flightplannerserver.domain.entities.CountryEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -7,5 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CountryRepository extends CrudRepository<CountryEntity, Long> {
 
+    CountryEntity findCountryEntityByCountryName(String countryName);
     boolean existsByCountryName(String countryName);
 }
