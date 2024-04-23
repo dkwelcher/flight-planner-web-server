aebf0f99e257a839288bcc9fbfadb375954dbeba
dkwelcher <dkwelcher@gmail.com>
2024-04-23 14:34:39 -0400

    Added method for checking if airport already exists by airport name

--- src/main/java/com/flightplanner/flightplannerserver/repositories/AirportRepository.java
+++ src/main/java/com/flightplanner/flightplannerserver/repositories/AirportRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AirportRepository extends CrudRepository<AirportEntity, Long> {
+
+    boolean existsByAirportName(String airportName);
 }
