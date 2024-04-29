f736e61890066f4c6553610acc2a4f43c4f9dec2
dkwelcher <dkwelcher@gmail.com>
2024-04-29 10:06:14 -0400

    Added individual methods for finding airports by: icao, country name, whether jet-a is available, & whether avgas is available

--- src/main/java/com/flightplanner/flightplannerserver/repositories/AirportRepository.java
+++ src/main/java/com/flightplanner/flightplannerserver/repositories/AirportRepository.java
@@ -1,11 +1,25 @@
 package com.flightplanner.flightplannerserver.repositories;
 
 import com.flightplanner.flightplannerserver.domain.entities.AirportEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AirportRepository extends CrudRepository<AirportEntity, Long> {
 
-    boolean existsByAirportName(String airportName);
+    boolean existsByIcao(String icao);
+
+    Optional<AirportEntity> findByIcao(String icao);
+    @Query("SELECT a FROM AirportEntity a WHERE a.country.countryName = :countryName")
+    List<AirportEntity> findAllByCountryName(String countryName);
+
+    @Query("SELECT a FROM AirportEntity a WHERE a.isJetaAvailable = true")
+    List<AirportEntity> findAllByIsJetaAvailable();
+
+    @Query("SELECT a FROM AirportEntity a WHERE a.isAvgasAvailable = true")
+    List<AirportEntity> findAllByIsAvgasAvailable();
 }
