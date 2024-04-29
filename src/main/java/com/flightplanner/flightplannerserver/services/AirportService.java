3c87aa88afd27491e6e15e0c75379892cace2b71
dkwelcher <dkwelcher@gmail.com>
2024-04-29 10:06:17 -0400

    Added individual methods for finding airports by: icao, country name, whether jet-a is available, & whether avgas is available

--- src/main/java/com/flightplanner/flightplannerserver/services/AirportService.java
+++ src/main/java/com/flightplanner/flightplannerserver/services/AirportService.java
@@ -11,6 +11,10 @@ public interface AirportService {
     AirportDto save(AirportDto airportDto);
     AirportDto update(AirportDto airportDto);
     Optional<AirportDto> findById(Long airportId);
+    Optional<AirportDto> findByIcao(String icao);
+    List<AirportDto> findAllByCountryName(String countryName);
+    List<AirportDto> findAllByIsJetaAvailable();
+    List<AirportDto> findAllByIsAvgasAvailable();
     List<AirportDto> findAll();
     boolean existsById(Long airportId);
     void deleteById(Long airportId);
