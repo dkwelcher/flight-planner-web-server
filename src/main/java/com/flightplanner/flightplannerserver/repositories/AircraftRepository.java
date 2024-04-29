82d58c0ed36ae6a81d1fd704e3f75d858ebae218
dkwelcher <dkwelcher@gmail.com>
2024-04-29 10:04:38 -0400

    Added method to list aircraft by make. Added method to list aircraft by type

--- src/main/java/com/flightplanner/flightplannerserver/repositories/AircraftRepository.java
+++ src/main/java/com/flightplanner/flightplannerserver/repositories/AircraftRepository.java
@@ -4,6 +4,12 @@ import com.flightplanner.flightplannerserver.domain.entities.AircraftEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AircraftRepository extends CrudRepository<AircraftEntity, Long> {
+
+    List<AircraftEntity> findAllByAircraftMake(String aircraftMake);
+
+    List<AircraftEntity> findAllByAircraftType(String aircraftType);
 }
