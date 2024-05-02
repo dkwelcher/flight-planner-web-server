3cd3751e91cce55f27e713a07942b915ee43692f
dkwelcher <dkwelcher@gmail.com>
2024-05-02 12:28:16 -0400

    Added documentation of class & methods

--- src/main/java/com/flightplanner/flightplannerserver/controllers/AircraftController.java
+++ src/main/java/com/flightplanner/flightplannerserver/controllers/AircraftController.java
@@ -2,13 +2,18 @@ package com.flightplanner.flightplannerserver.controllers;
 
 import com.flightplanner.flightplannerserver.domain.dto.AircraftDto;
 import com.flightplanner.flightplannerserver.services.AircraftService;
-import lombok.extern.java.Log;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+
+/**
+ * Controller for handling aircraft-related operations.
+ *
+ * @since 2024-04-29
+ */
 
 @RestController
 public class AircraftController {
@@ -19,6 +24,12 @@ public class AircraftController {
         this.aircraftService = aircraftService;
     }
 
+    /**
+     * Creates a new aircraft record.
+     *
+     * @param aircraftDto Data transfer object representing the aircraft to be created.
+     * @return A ResponseEntity containing the created aircraft or an error status.
+     */
     @PostMapping(path = "/aircraft")
     public ResponseEntity<AircraftDto> createAircraft(@RequestBody AircraftDto aircraftDto) {
 
@@ -26,6 +37,13 @@ public class AircraftController {
         return new ResponseEntity<>(savedAircraftDto, HttpStatus.CREATED);
     }
 
+    /**
+     * Updates an existing aircraft record.
+     *
+     * @param aircraftId The ID of the aircraft to update.
+     * @param aircraftDto Data transfer object representing the updated aircraft details.
+     * @return A ResponseEntity containing the updated aircraft or an error status.
+     */
     @PutMapping(path = "/aircraft/{aircraftId}")
     public ResponseEntity<AircraftDto> updateAircraft(
             @PathVariable("aircraftId") long aircraftId,
@@ -43,6 +61,12 @@ public class AircraftController {
         return new ResponseEntity<>(updatedAircraftDto, HttpStatus.OK);
     }
 
+    /**
+     * Retrieves an AircraftDto of a specific ID if the specified aircraft exists.
+     *
+     * @param aircraftId The ID of the aircraft being queried.
+     * @return A ResponseEntity containing the queried aircraft or an HTTP 404 Not Found status, otherwise a ResponseEntity containing an error status.
+     */
     @GetMapping(path = "/aircraft/{aircraftId}")
     public ResponseEntity<AircraftDto> findAircraftById(@PathVariable("aircraftId") long aircraftId) {
 
@@ -52,18 +76,35 @@ public class AircraftController {
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    /**
+     * Retrieves a list of aircraft with matching aircraft make (e.g., Boeing or Airbus).
+     *
+     * @param aircraftMake The String representation of the aircraft make being queried.
+     * @return A ResponseEntity containing a list of AircraftDtos or an error status.
+     */
     @GetMapping(path = "/aircraft/make/{aircraftMake}")
     public ResponseEntity<List<AircraftDto>> listAllAircraftByMake(@PathVariable("aircraftMake") String aircraftMake) {
         List<AircraftDto> aircraftDtos = aircraftService.findAllByAircraftMake(aircraftMake);
         return new ResponseEntity<>(aircraftDtos, HttpStatus.OK);
     }
 
+    /**
+     * Retrieves a list of aircraft with matching aircraft type (e.g., Jet, Turboprop, or Prop).
+     *
+     * @param aircraftType The String representation of the aircraft type being queried.
+     * @return A ResponseEntity containing a list of AircraftDtos or an error status.
+     */
     @GetMapping(path = "/aircraft/type/{aircraftType}")
     public ResponseEntity<List<AircraftDto>> listAllAircraftByType(@PathVariable("aircraftType") String aircraftType) {
         List<AircraftDto> aircraftDtos = aircraftService.findAllByAircraftType(aircraftType);
         return new ResponseEntity<>(aircraftDtos, HttpStatus.OK);
     }
 
+    /**
+     * Retrieves a list of all aircraft.
+     *
+     * @return A ResponseEntity containing a list of AircraftDtos or an error status.
+     */
     @GetMapping(path = "/aircraft/all")
     public ResponseEntity<List<AircraftDto>> listAllAircraft() {
 
@@ -71,6 +112,12 @@ public class AircraftController {
         return new ResponseEntity<>(aircraftDtos, HttpStatus.OK);
     }
 
+    /**
+     * Deletes an aircraft record.
+     *
+     * @param aircraftId The ID of the aircraft to be deleted.
+     * @return A ResponseEntity indicating the HTTP status result of the delete operation.
+     */
     @DeleteMapping(path = "/aircraft/{aircraftId}")
     public ResponseEntity<HttpStatus> deleteAircraft(@PathVariable("aircraftId") long aircraftId) {
 
--- src/main/java/com/flightplanner/flightplannerserver/controllers/AirportController.java
+++ src/main/java/com/flightplanner/flightplannerserver/controllers/AirportController.java
@@ -2,13 +2,18 @@ package com.flightplanner.flightplannerserver.controllers;
 
 import com.flightplanner.flightplannerserver.domain.dto.AirportDto;
 import com.flightplanner.flightplannerserver.services.AirportService;
-import lombok.extern.java.Log;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+
+/**
+ * Controller for handling airport-related operations.
+ *
+ * @since 2024-04-29
+ */
 
 @RestController
 public class AirportController {
@@ -17,12 +22,25 @@ public class AirportController {
 
     public AirportController(AirportService airportService) { this.airportService = airportService; }
 
+    /**
+     * Creates a new airport record.
+     *
+     * @param airportDto Data transfer object representing the airport to be created.
+     * @return A ResponseEntity containing the created airport or an error status.
+     */
     @PostMapping(path = "/airports")
     public ResponseEntity<AirportDto> createAirport(@RequestBody AirportDto airportDto) {
         AirportDto savedAirportDto = airportService.save(airportDto);
         return new ResponseEntity<>(savedAirportDto, HttpStatus.CREATED);
     }
 
+    /**
+     * Updates an existing airport record.
+     *
+     * @param airportId The ID of the airport to update.
+     * @param airportDto Data transfer object representing the updated airport details.
+     * @return A ResponseEntity containing the updated airport or an error status.
+     */
     @PutMapping(path = "/airports/{airportId}")
     public ResponseEntity<AirportDto> updateAirport(
             @PathVariable("airportId") long airportId,
@@ -40,6 +58,12 @@ public class AirportController {
         return new ResponseEntity<>(updatedAirportDto, HttpStatus.OK);
     }
 
+    /**
+     * Retrieves an AirportDto of a specific ID if the specified airport exists.
+     *
+     * @param airportId The ID of the airport being queried.
+     * @return A ResponseEntity containing the queried airport or an HTTP 404 Not Found status, otherwise a ResponseEntity containing an error status.
+     */
     @GetMapping(path = "/airports/{airportId}")
     public ResponseEntity<AirportDto> findAirportById(@PathVariable("airportId") long airportId) {
         Optional<AirportDto> airportDto = airportService.findById(airportId);
@@ -48,6 +72,12 @@ public class AirportController {
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    /**
+     * Retrieves an AirportDto of a specific ICAO if the specified airport exists.
+     *
+     * @param icao The String representation of the ICAO code of the airport being queried.
+     * @return A ResponseEntity containing the queried airport or an HTTP 404 Not Found status, otherwise a ResponseEntity containing an error status.
+     */
     @GetMapping(path = "/airports/icao/{icao}")
     public ResponseEntity<AirportDto> findAirportByIcao(@PathVariable("icao") String icao) {
         Optional<AirportDto> airportDto = airportService.findByIcao(icao);
@@ -56,30 +86,57 @@ public class AirportController {
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    /**
+     * Retrieves a list of airports with matching country name (e.g., United States, Germany, or China).
+     *
+     * @param countryName The String representation of the country of airports being queried.
+     * @return A ResponseEntity containing a list of AirportDtos or an error status.
+     */
     @GetMapping(path = "/airports/country/{countryName}")
     public ResponseEntity<List<AirportDto>> listAllAirportsByCountryName(@PathVariable("countryName") String countryName) {
         List<AirportDto> airportDtos = airportService.findAllByCountryName(countryName);
         return new ResponseEntity<>(airportDtos, HttpStatus.OK);
     }
 
+    /**
+     * Retrieves a list of airports that provide the Jet-A fuel type.
+     *
+     * @return A ResponseEntity containing a list of AirportDtos or an error status.
+     */
     @GetMapping(path = "/airports/jeta")
     public ResponseEntity<List<AirportDto>> listAllAirportsByIsJetaAvailable() {
         List<AirportDto> airportDtos = airportService.findAllByIsJetaAvailable();
         return new ResponseEntity<>(airportDtos, HttpStatus.OK);
     }
 
+    /**
+     * Retrieves a list of airports that provide the Avgas fuel type.
+     *
+     * @return A ResponseEntity containing a list of AirportDtos or an error status.
+     */
     @GetMapping(path = "/airports/avgas")
     public ResponseEntity<List<AirportDto>> listAllAirportsByIsAvgasAvailable() {
         List<AirportDto> airportDtos = airportService.findAllByIsAvgasAvailable();
         return new ResponseEntity<>(airportDtos, HttpStatus.OK);
     }
 
+    /**
+     * Retrieves a list of all airports.
+     *
+     * @return A ResponseEntity containing a list of AirportDtos or an error status.
+     */
     @GetMapping(path = "/airports/all")
     public ResponseEntity<List<AirportDto>> listAllAirports() {
         List<AirportDto> airportDtos = airportService.findAll();
         return new ResponseEntity<>(airportDtos, HttpStatus.OK);
     }
 
+    /**
+     * Deletes an airport record.
+     *
+     * @param airportId The ID of the airport to be deleted.
+     * @return A ResponseEntity indicating the HTTP status result of the delete operation.
+     */
     @DeleteMapping(path = "/airports/{airportId}")
     public ResponseEntity<HttpStatus> deleteAirport(@PathVariable("airportId") long airportId) {
         airportService.deleteById(airportId);
