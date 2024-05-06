a80469433283322c054ae1abb02985146c5714e8
dkwelcher <dkwelcher@gmail.com>
2024-05-06 12:44:14 -0400

    Added documentation of interface for mapping between two objects

--- src/main/java/com/flightplanner/flightplannerserver/mappers/Mapper.java
+++ src/main/java/com/flightplanner/flightplannerserver/mappers/Mapper.java
@@ -1,8 +1,29 @@
 package com.flightplanner.flightplannerserver.mappers;
 
+/**
+ * Interface defining the mapping operations between two object types.
+ *
+ * @param <A> The source type from which the mapping is done.
+ * @param <B> The target type to which the mapping is done.
+ *
+ * @since 2024-04-19
+ */
+
 public interface Mapper<A, B> {
 
+    /**
+     * Maps an object of type A to an object of type B.
+     *
+     * @param a The source object to map from.
+     * @return The mapped object of type B.
+     */
     B mapTo(A a);
 
+    /**
+     * Maps an object of type B back to an object of type A.
+     *
+     * @param b The source object to map from.
+     * @return The mapped object of type A.
+     */
     A mapFrom(B b);
 }
