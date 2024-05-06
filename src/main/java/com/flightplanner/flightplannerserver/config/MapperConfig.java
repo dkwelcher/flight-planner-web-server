def27cb95c8ef872a228bdf6c4e16439a5faf35d
dkwelcher <dkwelcher@gmail.com>
2024-05-06 12:46:07 -0400

    Added documentation of mapper config for ModelMapper

--- src/main/java/com/flightplanner/flightplannerserver/config/MapperConfig.java
+++ src/main/java/com/flightplanner/flightplannerserver/config/MapperConfig.java
@@ -5,9 +5,23 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Configuration class for ModelMapper to define bean configurations.
+ *
+ * @since 2024-04-19
+ */
+
 @Configuration
 public class MapperConfig {
 
+    /**
+     * Creates a ModelMapper bean with a specified matching strategy.
+     *
+     * This bean is configured to use the LOOSE matching strategy, which provides
+     * a more lenient mapping of source and destination properties.
+     *
+     * @return A configured ModelMapper instance.
+     */
     @Bean
     public ModelMapper modelMapper() {
         ModelMapper modelMapper = new ModelMapper();
